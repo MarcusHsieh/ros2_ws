d2872e9801078759a51ae11ca893e99f9f42c96d
MarcusHsieh <marcus.j.hsieh@gmail.com>
2025-03-25 17:28:06 -0700

    Test package

---
+++ build/my_example_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mj/_Projects/ros2_ws/src/my_example_pkg
+# Build directory: /home/mj/_Projects/ros2_ws/build/my_example_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
